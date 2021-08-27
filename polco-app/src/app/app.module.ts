7017371ba4a33837138481e666f0360022afe078
savisch <swsolns@gmail.com>
2021-08-26 22:04:44 -0500

    Adds Http and Student Component

--- polco-app/src/app/app.module.ts
+++ polco-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentDisplayComponent } from './students/student-display/student-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentDisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
