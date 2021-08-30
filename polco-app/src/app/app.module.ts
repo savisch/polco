5c24b13229b7fe64b1b031ba3e087f5690cd9c64
savisch <swsolns@gmail.com>
2021-08-30 11:57:28 -0500

    Adds FormsModule

--- polco-app/src/app/app.module.ts
+++ polco-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { StudentDisplayComponent } from './students/student-display/student-disp
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
