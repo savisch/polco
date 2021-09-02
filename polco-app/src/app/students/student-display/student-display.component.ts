9466dfc20c4dcb416d4aefd109b23f4ccc9dc1f5
savisch <swsolns@gmail.com>
2021-09-02 16:14:33 -0500

    Adds fxn for tag input and display

--- polco-app/src/app/students/student-display/student-display.component.ts
+++ polco-app/src/app/students/student-display/student-display.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { StudentService } from 'src/app/service/student.service';
-import { Student } from 'src/app/models/student'
+import { Student } from 'src/app/model/student'
+import { Student2 } from 'src/app/model/student2';
 
 @Component({
   selector: 'app-student-display',
@@ -12,22 +13,24 @@ export class StudentDisplayComponent implements OnInit {
   constructor(private studentService: StudentService) { }
 
   students: Student[] = [];
+  mockStudents: Student2[] = [];
   average: number =  0;
   fullName: string = "";
 
   searchName: string = "";
   tagName: string = "";
-  addedTag: string = "";
   tags: string[] = [];
   filteredStudents: Student[] = [];
   filteredLength: number = 0;
 
+  match: boolean = false;
+
 
   getStudents(): void {
     this.studentService.getStudents().subscribe(gottenStudents => {
-      console.log(gottenStudents)
+      // console.log(gottenStudents)
       this.students = gottenStudents.students
-      console.log(this.students)
+      // console.log(this.students)
       this.getAverage()
       this.getFullName(this.students)
     })
@@ -40,9 +43,9 @@ export class StudentDisplayComponent implements OnInit {
   }
 
   filterStudents(): void {
-    console.log(this.searchName)
+    // console.log(this.searchName)
     this.filteredStudents = this.students.filter(student => student.fullName.toLowerCase().includes(this.searchName.toLowerCase()))
-    console.log (this.filteredStudents)
+    // console.log (this.filteredStudents)
     this.filteredLength = this.filteredStudents.length
     this.getFullName(this.filteredStudents)
   }
@@ -51,10 +54,12 @@ export class StudentDisplayComponent implements OnInit {
     for (let i = 0; i < this.students.length; i++) {
       let total: number = 0;
       let gLength: number = this.students[i].grades.length;
+
       for (let j = 0; j < gLength; j++) {
         let grade: number = Number(this.students[i].grades[j])
         total += grade
       }
+
       let average: number = total / gLength
       this.students[i].average = average
     }
@@ -74,7 +79,7 @@ export class StudentDisplayComponent implements OnInit {
 
   closeDetails(i: number): void {
     console.log("closed")
-    this.students[i].isOpen = false;
+    this.students[i].isOpen = false
   }
 
   filteredCloseDetails(i: number): void {
@@ -82,27 +87,52 @@ export class StudentDisplayComponent implements OnInit {
     this.filteredStudents[i].isOpen = false;
   }
 
-  getTag(i: number, value: string): void {
-    this.students[i].tags = []
-    console.log(this.students[i].tags)
-    this.students[i].tags.push(value)
-    // this.tags.push(value)
-    // console.log(this.tags)
-    // this.students[i].tags.push(...this.tags) 
-    // this.tags = []
+  getTag(i: number, value: string, name: string): void {
+    this.tags.push(value)
+    this.studentService.getMockStudents().subscribe(gottenStudents => {
+      this.mockStudents = gottenStudents
+      // console.log(this.mockStudents)
 
-    
+      if(this.mockStudents.length > 0) {
+        let studentMatch = this.mockStudents.find(student => student.fullName == name);
+        console.log(studentMatch)
+
+        if(studentMatch) {
+          console.log("match")
+          console.log("tags:" ,studentMatch.tags)
+          this.tags = studentMatch.tags
+          this.tags.push(value)
+          console.log("tags2:", this.tags)
+          
+          if(this.filteredStudents.length > 0) {
+            this.filteredStudents[i].tags = this.tags
+          } else {
+            this.students[i].tags = this.tags
+          }
+
+          this.studentService.putTag(studentMatch.fullName, studentMatch.studentId, studentMatch.id, this.tags).subscribe(result => {
+            console.log(result)
+          })
+          this.tags=[]
+
+        }else {
+          console.log("no match")
+          console.log(this.tags)
+
+          if(this.filteredStudents.length > 0) {
+            this.filteredStudents[i].tags = this.tags
+          } else {
+            this.students[i].tags = this.tags
+          }
+
+          this.studentService.newPostTag(this.students[i].fullName, i + 1, this.tags).subscribe(gottenTag => {
+            console.log(gottenTag)
+          })
+          this.tags=[]
+        }
+      }      
+    })   
   }
-
-  // addTags(i: number): void {
-  //   let timeout: any = null;
-  //   clearTimeout(timeout);
-  //   timeout = setTimeout(() => {
-  //     this.students[i].tags.push(this.addedTag)
-  //     console.log("Tag:", this.students[i].tags)
-  //   }, 2000);
-  // }
-  
 
   ngOnInit(): void {
     this.getStudents()
